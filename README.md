807081252503b86d88023f2f17860303258f9320
Onetonio <antonioneto3260@gmail.com>
2024-09-14 11:26:31 -0300

    Final

---
+++ README.md
@@ -0,0 +1,21 @@
+# Cafeteria-Java
+
+Sistema de Cafeteria em Java com Registro de Logs e Salvamento por Serialização
+ <h1>Para rodar este projeto Java, siga estas etapas:</h1>
+<h2>1. Clone o Repositório</h2>
+No seu terminal, execute o seguinte comando para clonar o repositório: git clone https://github.com/anton-neto/JornadaDaConquista
+
+<h2>2. Abra o Projeto no IntelliJ IDEA ou Eclipse</h2>
+Abra a sua IDE (IntelliJ IDEA, Eclipse, ou outra de sua preferência). Selecione "Open or Import" e navegue até a pasta do projeto clonado. Clique em "Open" ou "Import" para carregar o projeto.
+
+<h2>3. Configure o Ambiente</h2>
+Certifique-se de que você tem o JDK 11 ou superior instalado. Verifique também se as bibliotecas necessárias para o projeto estão configuradas corretamente.
+
+<h2>4. Compile o Projeto</h2>
+Na sua IDE, execute a compilação do projeto para garantir que todas as dependências sejam resolvidas e que o código esteja pronto para ser executado.
+
+<h2>5. Execute o Projeto</h2>
+Após a compilação bem-sucedida, execute o projeto. Dependendo da sua IDE, você pode fazer isso clicando com o botão direito na classe principal (geralmente Main.java) e selecionando "Run" ou utilizando o botão de "play" da IDE.
+
+<h2>6. Teste o Aplicativo</h2>
+Navegue pelo sistema de cafeteria e teste as funcionalidades de registro de pedidos, registro de logs e o salvamento de dados por serialização. O sistema deve salvar os dados automaticamente ao encerrar e permitir carregar os dados na próxima execução.
---
+++ src/App.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+import view.*;
+import util.Serializador;
+import controller.Cafeteria;
+
+//Equipe Cafeteria
+//ANTÔNIO ADAIR CABREIRA NETO - 8833702233
+//DAVI ALVES DE LINS - 8834109170
+//GABRIEL GELBCKE DA SILVA - 8833957134
+//GUSTAVO TORRES GIROTO - 8834184341
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Cafeteria cafeteria = Serializador.ler();
+
+        if(cafeteria == null){
+            cafeteria = new Cafeteria();
+        }
+
+        Scanner in = new Scanner(System.in);
+        int escolha = 0;
+        boolean menu = true;
+
+        while (menu){
+            try{
+            System.out.println("Escolha uma categoria: \n" + 
+            "1 - Produtos \n" +
+            "2 - Insumos \n" +
+            "3 - Usuarios \n" +
+            "4 - Funcionarios \n" +
+            "5 - Vendas \n" +
+            "0 - Sair");
+            escolha = in.nextInt();
+        } catch(Exception e) {
+            escolha = 999;
+            in.nextLine();
+        }
+            switch (escolha) {
+                case 1:
+                    IProdutos.menu(cafeteria);
+                    Serializador.gravar(cafeteria);
+                    break;
+                case 2:
+                    IInsumos.menu(cafeteria);
+                    Serializador.gravar(cafeteria);
+                    break;
+                case 3:
+                    IUsuarios.menu(cafeteria);
+                    Serializador.gravar(cafeteria);
+                    break;
+                case 4:
+                    IFuncionarios.menu(cafeteria);
+                    Serializador.gravar(cafeteria);
+                    break;
+                case 5:
+                    IVendas.menu(cafeteria);
+                    Serializador.gravar(cafeteria);
+                    break;
+                case 0:
+                    menu = false;
+                    break;
+                case 999:
+                    System.out.println("Input fora do range!");
+                    break;
+                default:
+                    System.out.println("Input fora do range!");
+                    break;
+            }
+        }
+            Serializador.gravar(cafeteria);
+    }
+}
